d84c44286752b3632b02df8a33e9bc7cb0df52c3
Sasha <79185842375@yandex.ru>
2021-10-28 13:31:41 +0300

    Готовое дз урок 8

---
+++ Основы языка пайтон/8_4.py
@@ -0,0 +1,26 @@
+from functools import wraps
+def val_checker(ch=0):
+    def _cheker(calc_cube):
+
+        @wraps(calc_cube)
+        def chek(x):
+            if ch(x) == 1:
+                msg = 'Меньше нуля'
+                raise ValueError(msg)
+            else:
+                return calc_cube(x)
+        return chek
+    return _cheker
+
+
+
+@val_checker(lambda x: x<0)
+def calc_cube(x):
+   return x ** 3
+
+a = calc_cube(3)
+print(a)
+a = calc_cube(2)
+print(a)
+a = calc_cube(-6)
+print(a)
